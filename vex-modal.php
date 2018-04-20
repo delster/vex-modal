c9f87874daa52ae60bc42d3a180c86691156c13e
David Elster <davidandtheweb@gmail.com>
2018-04-20 00:49:26 -0400

    Initial commit

---
+++ vex-modal.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ *  Plugin Name:  Vex Modal
+ *  Plugin URI:   https://github.com/delster/vex-modal
+ *  Description:  Basic Plugin Wrapper for vex Modal.
+ *  Version:      1.0
+ *  Author:       David Elster
+ *  Author URI:   https://github.com/delster/
+ *  License:      GPL2
+ *  License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ */
+defined( 'ABSPATH' ) or die( 'No peeking!' );
