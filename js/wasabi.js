ca48f4d9426b354ae82c9174dfc7c4870d5634f3
David Elster <davidandtheweb@gmail.com>
2018-04-20 21:02:21 -0400

    Production fixes

--- js/wasabi.js
+++ js/wasabi.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
   // Store our modal trigger DOM element.
-  const clientModalTrigger = document.querySelector("#client-modal");
+  const clientModalTrigger = document.querySelector(".clients-link");
 
   // On click, fire the modal.
   clientModalTrigger.addEventListener("click", () => {
@@ -8,15 +8,19 @@ jQuery(document).ready(function($) {
     setTimeout(() => {
       vex.dialog.prompt({
         className: "vex-theme-default",
-        unsafeMessage: `<h6>client login</h6>
+        unsafeMessage: `
+        <span id="close-vex" onClick="vex.closeAll()">X</span>
+        <h6>client login</h6>
         <p>For personal privacy, client galleries are password protected. To login, the password is "YYYYMMDDlastname," where the date is the day of the shoot (wedding day or engagement session) and last name can be either last name. Passwords are case-sensitive, and names are always lowercase.</p>
         <p>For example, the password for a July 3, 2018 wedding between John Smith and Sarah Kline would be either <strong>20180703smith</strong> or <strong>20180703kline</strong>.</p>`,
         placeholder: "20180703kline",
         buttons: [$.extend({}, vex.dialog.buttons.YES, { text: "Submit" })],
         callback: function(value) {
-          location.href =
-            "http://www.wasabiphotography.com/galleries/" +
-            document.querySelector(".vex-dialog-prompt-input").value;
+           if (value) {
+            location.href =
+              "http://www.wasabiphotography.com/galleries/" +
+              document.querySelector(".vex-dialog-prompt-input").value;
+          }
           return false;
         }
       });
